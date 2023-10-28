dc6adefe92bd149b8afc4cb62a49da2d73503422
Its-Arup <aruppaul1650@gmail.com>
2023-10-29 01:39:59 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import SignUp from './Components/SignUp';
-import AllRoutes from './Components/AllRoutes';
-import Navbar from './Components/Navbar';
+import "./App.css";
+import AllRoutes from "./Components/AllRoutes";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div>
       <h1>Notes Taking Application</h1>
-      <Navbar/>
-      <AllRoutes/>
+      <Navbar />
+      <AllRoutes />
     </div>
   );
 }
